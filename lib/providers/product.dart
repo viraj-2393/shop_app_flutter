8cc7303cb4ce4ebf6dcc5062faff6d3ab309c573
viraj-2393 <viraj.sased@gmail.com>
2022-03-08 20:58:44 +0530

    Optimistic favorite status updating added

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 class Product with ChangeNotifier{
   final String id;
   final String title;
@@ -16,8 +17,24 @@ class Product with ChangeNotifier{
     required this.imageUrl,
     this.isFavorite = false
 });
-  void toggleFavoriteStatus(){
+  void toggleFavoriteStatus() async{
+    final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
+    final url = Uri.parse('https://shop-app-f3f7c-default-rtdb.firebaseio.com/products/$id.json');
+    try {
+      final response = await http.patch(url, body: json.encode({
+        'isFavorite': isFavorite
+      }));
+      if(response.statusCode >= 400){
+        isFavorite = oldStatus;
+        notifyListeners(); 
+      }
+    }
+    catch(error){
+      isFavorite = oldStatus;
+      notifyListeners();
+    }
+
   }
 }
