42a0edc85316fbf0528d25256418983794c2d327
viraj-2393 <viraj.sased@gmail.com>
2022-03-11 09:30:17 +0530

    Login and signup screen added

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import './providers/orders.dart';
 import './screens/orders_screen.dart';
 import './screens/user_products_screen.dart';
 import './screens/edit_product_screen.dart';
+import './screens/auth_screen.dart';
 void main() {
   runApp(MyApp());
 }
@@ -36,13 +37,15 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.purple,
         ),
-        home: ProductOverviewScreen(),
+        home: AuthScreen(),//ProductOverviewScreen(),
         routes: {
+
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
           CartScreen.rounteName: (ctx) => CartScreen(),
           OrdersScreen.routeName: (ctx) => OrdersScreen(),
           UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
-          EditProductScreen.routeName: (ctx) =>  EditProductScreen()
+          EditProductScreen.routeName: (ctx) =>  EditProductScreen(),
+
         },
 
       ),
